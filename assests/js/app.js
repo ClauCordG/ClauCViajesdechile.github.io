4f38a4abcab275abbd9b4355df5574a0518356b4
ClauCordG <clmcgr@gmail.com>
2023-04-24 01:19:57 -0400

    codigo ayuda

--- assests/js/app.js
+++ assests/js/app.js
@@ -1,2 +1,10 @@
 const carousel = new bootstrap.Carousel('#myCarousel')
-
+$(document).ready(function(){
+    $('.carousel').slick({
+      autoplay: true,
+      autoplaySpeed: 2000,
+      arrows: true,
+      dots: false
+    });
+  });
+  
