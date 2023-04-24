cbacb75f39e006d6e305a45e001fd104fa37ca29
ClauCordG <clmcgr@gmail.com>
2023-04-24 03:45:01 -0400

    solo faltan las rss

--- assests/js/app.js
+++ assests/js/app.js
@@ -1,11 +1,4 @@
-<script>
-  // Inicializar el carrusel
-  var myCarousel = document.querySelector('#carouselExampleInterval')
-  var carousel = new bootstrap.Carousel(myCarousel, {
-    interval: 2000,
-    wrap: true
-  })
-</script>
+
 
 
   
