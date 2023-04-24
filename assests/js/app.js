f6d15c5ab6acbb96efe7c193235897b975a645b4
ClauCordG <clmcgr@gmail.com>
2023-04-24 03:15:16 -0400

    filas <3

--- assests/js/app.js
+++ assests/js/app.js
@@ -1,11 +1,3 @@
-const carousel = new bootstrap.Carousel('#myCarousel')
-$('.carousel').carousel();
-$(document).ready(function(){
-    $('.carousel').slick({
-      autoplay: true,
-      autoplaySpeed: 2000,
-      arrows: true,
-      dots: false
-    });
-  });
+
+
   
